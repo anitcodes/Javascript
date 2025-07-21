d43437d99065c20a68c6164348bf0f5d2c0a3d15
Anit Shrestha <anitshrestha101@gmail.com>
2025-07-21 23:09:27 +0545

    day1 done

---
+++ DOM/part1/dom1.js
@@ -0,0 +1,25 @@
+//DOM Manipulation part 1
+
+//1.Selecting element: getElementById, getElementByClassName, querySelector, querySelectorALl
+// let heading = document.querySelector("h1");
+// heading.textContent = "My name is Anit Shrestha"
+// heading.innerHTML = "<i>Coding</i>"
+// console.log(heading)
+// heading.hidden = true;
+
+//2. Text/Content Access: getAttribute, setAttribute, removeAttribute
+// let a = document.querySelector("a");
+// a.href = "https://www.google.com"; //add links of google in a tag
+
+// let h2 = document.querySelector("h2");
+// h2.remove(); //removes element of h2
+
+
+// 3.
+// let h3 = document.createElement("h3");
+// h3.textContent = "This will come first";
+// document.querySelector("body").prepend(h3);
+
+// let h4 = document.createElement("h4");
+// h4.textContent = "This will come last";
+// document.querySelector("body").append(h4);
