7bd36e2496297bc8b314adc49125b20dc5f033e9
Anit Shrestha <anitshrestha101@gmail.com>
2025-08-09 11:57:35 +0545

    array methods

--- JS-Basic/array/array.js
+++ JS-Basic/array/array.js
@@ -64,7 +64,9 @@
 
 
 
-//Mastering the array methods
+//Mastering the most important array methods
+
+// 1.map()
 
 // array.map((item, index, array) => {
     //return the transformed value
@@ -82,6 +84,94 @@
 //     console.log("Array:",array);
 // })
 
-// using item
+// map() using item
 //we use item to work with the value itself
 
+// const prices = [10,20,30];
+// const doubled = prices.map(item => item*2);
+// console.log(doubled)
+
+// map() using index
+// const colors = ["red", "green", "blue"];
+
+// const numberedColors = colors.map((color, index) => {
+//     return `${index+1} : ${color}`;
+// })
+// console.log(numberedColors)
+
+// map() using array
+// const marks = [50, 75, 100];
+
+// const percentages = marks.map((item, index, array) => {
+//     const total = array.reduce((sum, val) => sum + val, 0);
+//     return ((item / total) * 100).toFixed(1) + "%";
+// });
+
+// console.log(percentages);
+// ["22.2%", "33.3%", "44.4%"]
+
+// 2. filter()
+
+// Syntax
+// array.filter((item,index,array) =>{
+//     return condition; //true to keep, false to remobe
+// });
+
+// example:
+// const nums = [1,2,4,5,9,56,33];
+// const evenNum = nums.filter(n => n%2 == 0);
+// console.log(evenNum);
+
+// 3.find()
+
+// syntax
+// array.find((item,index,array) =>{
+//     return condition; //stops at first true
+// })
+
+// example
+// const users = [
+//     {id:1, name: "Anit"},
+//     {id:2, name: "Shila"},
+//     {id:3, name: "Mamata"}
+// ];
+
+// const user = users.find(u => u.name == "Mamata")
+// console.log(user)
+
+// 4.includes()
+// syntax
+// array.includes(valueToFind, fromIndex?);
+
+// example
+// const faculty = ["csit","bca","bit"]
+// console.log(faculty.includes("bca")); //true
+// console.log(faculty.includes("bhm")); //false
+
+// 5.reduce()
+// syntax
+// array.reduce((accumulator, currentValue, index, array) => {
+//   // return updated accumulator
+// }, initialValue);
+
+
+// example: sum of numbers
+// const numbers = [2,3,5];
+
+// const sum = numbers.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// }, 0);
+
+// console.log(sum); // 10
+
+
+// Use reduce() to count how many times each fruit appears:
+// const fruits = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
+
+// const count = fruits.reduce((acc, fruit) => {
+//   acc[fruit] = (acc[fruit] || 0) + 1;
+//   return acc;
+// }, {});
+
+// console.log(count);
+// // Output: { apple: 3, banana: 2, orange: 1 }
