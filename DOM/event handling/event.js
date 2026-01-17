c1eb2fa6970f9fbcca04a0b1fcb45cfa9ec3edb9
Anit Shrestha <anitshrestha101@gmail.com>
2026-01-17 15:59:23 +0545

    event handling practiced

--- DOM/event handling/event.js
+++ DOM/event handling/event.js
@@ -50,3 +50,85 @@
 // box.addEventListener("mouseout", function () {
 //   box.style.backgroundColor = "white";
 // });
+
+//Q.8
+// let para = document.querySelector("p");
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//   para.style.fontSize = "32px";
+// });
+
+//Q.9
+// let para = document.querySelector("p");
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//   if (para.style.display === "none") {
+//     para.style.display = "block";
+//     btn.textContent = "Hide";
+//   } else {
+//     para.style.display = "none";
+//     btn.textContent = "Show";
+//   }
+// });
+
+// Q.11
+// let count = document.querySelector("#count");
+// let btn = document.querySelector("#btn");
+
+// let number = 0;
+
+// btn.addEventListener("click", function(){
+//     number = number + 1;
+//     count.textContent = number;
+// })
+
+// Q.12
+// let button = document.getElementById("btn");
+
+// button.addEventListener("click", function () {
+//   button.textContent = "Clicked!";
+// });
+// button.addEventListener("dblclick", function () {
+//   button.textContent = "Double Clicked!!";
+// });
+
+//Q.15
+// let toogle = document.getElementById("toogle");
+// let body = document.querySelector("body");
+
+// toogle.addEventListener("click", function(){
+//     body.style.backgroundColor = "black";
+// })
+
+// Q.16
+// let imgs = document.querySelectorAll(".container img");
+
+// imgs.forEach(function (img) {
+//   let originalSrc = img.getAttribute("src"); // store original image for each
+
+//   img.addEventListener("mouseover", function () {
+//     img.setAttribute(
+//       "src",
+//       "https://images.pexels.com/photos/16036169/pexels-photo-16036169.jpeg"
+//     );
+//   });
+
+//   img.addEventListener("mouseout", function () {
+//     img.setAttribute("src", originalSrc);
+//   });
+// });
+
+//Q.17
+// Select large display image
+// let display = document.getElementById("display");
+
+// // Select all thumbnails
+// let thumbs = document.querySelectorAll(".thumb");
+
+// // Loop through thumbnails and add click event
+// thumbs.forEach(function (thumb) {
+//   thumb.addEventListener("click", function () {
+//     // Change the large display image to clicked thumbnail
+//     display.src = thumb.src;
+//   });
+// });
